7dbbce182ea4e0fb8a285caef4e2fda99e2e2e84
raxit65535 <raxit65535@csu.fullerton.edu>
2019-06-27 23:11:54 -0700

    added UI component flavours and clean the code

--- Spark-Streaming/Location-hash.py
+++ Spark-Streaming/Location-hash.py
@@ -1,13 +1,13 @@
+from __future__ import print_function
 from pyspark import SparkContext
 from pyspark.streaming import StreamingContext
 from kafka import KafkaProducer
-# from pyspark.sql.functions import expr
 from pyspark.streaming.kafka import KafkaUtils
 import sys
 from datetime import datetime
 from math import radians, cos, sin, asin, sqrt
 import pygeohash as gh
-# from pyspark.sql.functions import expr
+
 
 '''
 Precision of Geo Hash
@@ -25,39 +25,29 @@ Precision of Geo Hash
 11  ± 0.000074
 
 '''
-
-
+# Function to generate geo location hash based on longitude and lattitude. Precision is 5 decimal points
 def geohash(x):
 
     array = x.strip("()").split(",")
-
     lon = array[0]
     lat = array[1]
-    # lon, lat = x[:]
-    # print(tuple((round(float(lon),5), round(float(lat),5))))
-
-    # return tuple((round(float(lon), 4), round(float(lat), 4)))
     return gh.encode(float(lon), float(lat), 5)
     # print(x)
 
 
+# function to Sink the computation statestics to Kafka
 def kafkaSink(matched_stats):
     producer = KafkaProducer(bootstrap_servers="localhost:9092")
     # producer = KafkaProducer(bootstrap_servers = "10.0.0.4:9092, 10.0.0.5:9092, 10.0.0.10:9092")
-    print(matched_stats)
     producer.send('hash_matched_stats', matched_stats.encode('utf8'))
     producer.flush()
 
-
+# Function to compute Herversine Distance for given two location points
 def haversine(lon1, lat1, lon2, lat2):
     """
     Calculate the great circle distance between two points
     on the earth (specified in decimal degrees)
     """
-
-    # lonlat1 = lonlat1.strip("()").split(",")
-    # lonlat2 = lonlat2.strip("()").split(",")
-
     lon1 = float(lon1)
     lat1 = float(lat1)
     lon2 = float(lon2)
@@ -74,6 +64,8 @@ def haversine(lon1, lat1, lon2, lat2):
     return c * r
 
 
+# function to format the message into semicolan seperated stacked values into a single variable
+# Helper function to sink the matched records to kafka
 def format_message_matched(ride_request_id, pickup_loc, drop_off_loc, water_mark_rider, driver_id, driver_loc, water_mark_driver, distance, now_timestamp):
     str_fmt = "{};{};{};{};{};{};{};{};{}"
     message = str_fmt.format(ride_request_id, pickup_loc, drop_off_loc, water_mark_rider,
@@ -82,13 +74,16 @@ def format_message_matched(ride_request_id, pickup_loc, drop_off_loc, water_mark
     return message
 
 
+# function to format the message into semicolan seperated stacked values into a single variable
+# Helper function to sink the updated driver location to kafka
 def format_message_driver(driver_id, driver_loc, timestamp):
     str_fmt = "{};{};{}"
     message = str_fmt.format(driver_id, driver_loc, timestamp)
     # print(message)
     return message
 
-
+# function to format the message into semicolan seperated stacked values into a single variable
+# Helper function to sink the matched stats to kafka
 def format_match_stats(total_ride_req, matched_riders, total_drivers, timestamp):
     str_fmt = "{};{};{};{}"
     message = str_fmt.format(total_ride_req, matched_riders,
@@ -97,7 +92,10 @@ def format_match_stats(total_ride_req, matched_riders, total_drivers, timestamp)
     return message
 
 
+# Process the RDD and compute Greedy bipartite match
 def process_union(rdd):
+
+    # Empty data structures to keep track of Rider and Driver ids who are matched. Avoid duplicates
     matched_list = dict()
     matched_driver = dict()
 
@@ -109,13 +107,18 @@ def process_union(rdd):
 
     unmatched_rider_list = []
     unmatched_driver_list = []
-    # print("raxit start")
+    
+    # for each record in RDD
     for line in rdd:
-
+        
+        # Since RDD record is a tuple, seperating it into two values rider and driver
         rider, driver = line[1]
-        # rider = rider.split(";")
-        # driver = driver.split(";")
-
+        
+        # When doing left outer join, if the hash values doen't match, the driver's tuple will contain none. 
+        # I define them as outliers. The if condition will ignore the outliers.
+        if(driver is None or rider is None):
+            continue
+        # Seperating out all the values from tuple into single variables.    
         ride_pickup = rider[1].strip("()").split(",")
         driver_loc = driver[1].strip("()").split(",")
         ride_long = ride_pickup[0]
@@ -123,11 +126,12 @@ def process_union(rdd):
         driver_long = driver_loc[0]
         driver_lat = driver_loc[1]
 
-        # print(rider[0], driver[0])
-
+        
+        # keeping the dictionary of Total riders
         if rider[0] not in total_ride_req:
             total_ride_req[rider[0]] = "new"
 
+        # keeping the dictionary of total drivers
         if driver[0] not in total_driver:
             total_driver[driver[0]] = "new"
 
@@ -140,14 +144,13 @@ def process_union(rdd):
                 # kafka_matched_sink = format_message_matched(
                 #     rider[0], rider[1], rider[2], rider[3], driver[0], driver[1], driver[2], distance, str(datetime.now()))
                 # sink_match.append(kafka_matched_sink)
-                # kafka_driver_sink = format_message_driver(
-                #     driver[0], rider[2], str(datetime.now()))
-                # sink_driver.append(kafka_driver_sink)
+                kafka_driver_sink = format_message_driver(
+                    driver[0], rider[2], str(datetime.now()))
+                sink_driver.append(kafka_driver_sink)
                 matched_driver[driver[0]] = "new"
 
         # print("raxit stop", line[1])
-    
-    
+
     # dont need this computation since we can figureout unmatched riders by using the matched riders and total stats
     # for key in matched_list:
     #     # print("raxit: iterating key", key)
@@ -162,7 +165,8 @@ def process_union(rdd):
 
     # if len(matched_list) != 0:
     # print("inserting value:", len(matched_list))
-    matched_stats = format_match_stats(len(total_ride_req), len(matched_list), len(total_driver), str(datetime.now()))
+    matched_stats = format_match_stats(len(total_ride_req), len(
+        matched_list), len(total_driver), str(datetime.now()))
     # print(matched_stats)
     kafkaSink(matched_stats)
 
@@ -171,47 +175,70 @@ def process_union(rdd):
     # print("total number of driver active in the window: ", len(total_driver))
     # print("number of drivers matched with rider in the window: ", len(matched_driver))
 
-    print(len(total_ride_req), len(matched_list))
-    print(len(total_driver), len(matched_driver))
-    print(len(sink_match), len(sink_driver))
-    print('--------------------')
+    # print(len(total_ride_req), len(matched_list))
+    # print(len(total_driver), len(matched_driver))
+    # print(len(sink_match), len(sink_driver))
+    # print('--------------------')
+    print('total Driver:', len(total_driver))
+    print('total ride req:', len(total_ride_req))
+
 
 def main():
 
+    #initializing the Spark Context
     sparkContext = SparkContext(appName='Bipartite-Matching')
-
     sparkContext.setLogLevel('ERROR')
     sparkStreamingContext = StreamingContext(sparkContext, 1)
 
-    # create DStream that reads from kafka topic
+    # create DStream that reads the Driver Stream
     spark_kafka_driver_Stream = KafkaUtils.createDirectStream(sparkStreamingContext, ['driver_topic'],
                                                               # {'metadata.broker.list': '10.0.0.4:9092, 10.0.0.5:9092, 10.0.0.10:9092'})
                                                               {'metadata.broker.list': 'localhost:9092'})
+    
+    #creating DStream that reads the Rider stream
     spark_kafka_rider_stream = KafkaUtils.createDirectStream(sparkStreamingContext, ['rider_topic'],
                                                              #  {'metadata.broker.list': '10.0.0.4:9092, 10.0.0.5:9092, 10.0.0.10:9092'})
                                                              {'metadata.broker.list': 'localhost:9092'})
 
+    
+    # defining the Window size
     driver_window = spark_kafka_driver_Stream.window(2)
-    rider_window = spark_kafka_rider_stream.window(2)
+    rider_window = spark_kafka_rider_stream.window(4)
 
+
+    # creating location hash - precision 0.5 on driver RDD.
     driver_window = driver_window.map(lambda x: x[1])\
         .map(lambda x: x.split(";"))\
         .map(lambda x: (geohash(x[1]), x))
-    # driver_window.pprint()
+    # print(driver_window.count())
 
+    # optional debugging: checking the counts of driver RDD
+    driver_count = driver_window.foreachRDD(
+        lambda x: print('driver:', x.count()))
+
+    # creating location hash - precision 0.5 on rider RDD.
     rider_window = rider_window.map(lambda x: x[1])\
         .map(lambda x: x.split(";"))\
         .map(lambda x: (geohash(x[1]), x))
 
-    # rider_window.pprint()
+    # optional debugging: checking the counts of Rider RDD
+    rider_count = rider_window.foreachRDD(lambda x: print('rider:', x.count()))
 
-    union_rdd = rider_window.join(driver_window)
 
-    # union_rdd.pprint()
+    # Geerdy match - left outer join on Driver RDD and Rider RDD on key location hash.
+    union_rdd = rider_window.leftOuterJoin(driver_window)
 
-    union_rdd = union_rdd.foreachRDD(
-        lambda rdd: rdd.foreachPartition(process_union))
+    # optional debugging: mantaining the count for Joined RDD
+    join_count = union_rdd.foreachRDD(lambda x: print('join:', x.count()))
 
+    # union_rdd = union_rdd.foreachRDD(
+    #     lambda rdd: rdd.foreachPartition(process_union))
+
+    
+    # optional Debugging: running without partitioning RDDs.
+    union_rdd = union_rdd.foreachRDD(lambda x: process_union(x))
+
+    
     sparkStreamingContext.start()
     sparkStreamingContext.awaitTermination()
 
